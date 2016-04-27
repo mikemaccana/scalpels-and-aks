f6da92cc6106cdbc925c842a18186107cb4ea131
Mike MacCana <mike@certsimple.com>
2016-04-27 08:00:57 +0100

    Original version (based on burn everything down and use flex) Use SASS

---
+++ README.md
@@ -0,0 +1,15 @@
+# NPM everywhere
+
+This talk is online at [npm-everywhere](http://mikemaccana.github.io/npm-everywhere)
+
+Mike is the founder of CertSimple, which does fast, painless EV SSL. He's built both server infrastructure and web apps for companies including Red Hat, IBM, Google,  Microsoft and a number of startups for the last 17 years.
+
+## Running
+
+	npm install
+
+	gulp
+
+## Credits
+
+Thanks to [Wes Bos](http://wesbos.com/) for his https://github.com/wesbos/Modern-Workflow-and-Tooling-Talk, the deck of which was used to produce this talk. This is in turn uses http://code.google.com/p/html5slides/.
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	p = require('gulp-load-plugins')();
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// Start the server
+gulp.task('browser-sync', function() {
+	browserSync({
+		open : true,
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+gulp.task('slides',function () {
+	gulp.src('index.jade')
+		.pipe(p.jade())
+		.pipe(gulp.dest('./'))
+		.pipe(reload({stream:true}))
+});
+
+
+gulp.task('styles',function() {
+	gulp.src('./styles.scss')
+		.pipe(sass())
+		.pipe(p.autoprefixer())
+		.pipe(gulp.dest('./'))
+		.pipe(reload({stream:true}))
+});
+
+
+gulp.task('default', ['slides','styles','browser-sync'] ,function() {
+	gulp.watch('./**/*.jade',['slides']);
+	gulp.watch('./**/*.scss',['styles']);
+});
